14073a2a8cf512ed8ef6d5cc6df9eaafcccf993e
Thiago <thiagobarros@cjr.org.br>
2018-10-22 09:01:02 -0300

    Criando o Comment model e controller, apenas com o atributo commentContent

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comment/create'
+  get 'comment/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "static_pages#index"
 end
