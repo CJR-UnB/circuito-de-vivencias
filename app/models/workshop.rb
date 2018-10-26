8909fd253b688164ab0e28d44a20b9558e4a816e
Vinícius Gomes <vgdsouza@live.com>
2018-10-26 14:54:05 -0300

    validacao dos arquivos funcionando apenas no front

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,8 +1,7 @@
 class Workshop < ApplicationRecord
   has_one :user, foreign_key: 'author_id'
   has_one :user, foreign_key: 'editor_id'
-  has_many_attached :documents
+  has_one_attached :document
 
   validates :title, presence: true
-  validates :document, attached: true
 end
