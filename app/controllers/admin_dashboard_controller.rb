3f29c3b637d80d8bd631bcfd25b637f223f50d98
Vinícius Gomes <vgdsouza@live.com>
2018-11-06 15:00:51 -0200

    cards dessa semana prontos, mas não consegui implementar o template

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -4,15 +4,55 @@ class AdminDashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 
-  def authenticate_admin
-    redirect_to(root_path) unless current_user.adminRole?
+  def home
   end
 
-  def home
+  def edit_user
+    @user = User.find(params[:id])
+  end
+
+  def update_user
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      flash[:notice] = "Usuário atualizado com sucesso!"
+      redirect_to adminDashboard_users_path
+    else
+      flash[:notice] = "Não foi possível atualizar o usuário!"
+      redirect_to adminDashboard_users_path
+    end
+  end
+
+  def delete_user
+    user = User.find(params[:id])
+    if user.destroy
+      flash[:notice] = "Usuário deletado com sucesso!"
+      redirect_to adminDashboard_users_path
+    else
+      flash[:alert] = "Não foi possível deletar o usuário!"
+      redirect_to adminDashboard_users_path
+    end
   end
 
   def users
     @users = User.order(:name)
+  end
+
+  private
+
+  def authenticate_admin
+    redirect_to(root_path) unless current_user.adminRole?
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :surname,
+      :cpf,
+      :adminRole,
+      :supervisorRole,
+      :userRole
+    )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   scope path: 'adminDashboard', as: 'adminDashboard' do
     get '/home', to: 'admin_dashboard#home'
     get '/users', to: 'admin_dashboard#users'
+    delete '/users', to: 'admin_dashboard#delete_user'
+    get '/edit_user/:id', to: 'admin_dashboard#edit_user', as: 'edit_user'
+    patch '/edit_user/:id', to: 'admin_dashboard#update_user', as: 'update_user'
   end
   get '/users', to: 'users/registrations#new'
   resources :workshops
