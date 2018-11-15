ef8d62aa023224ead6c27e0f2adf3f87c18e8519
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2018-11-15 19:36:14 -0200

    deploy fixes

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module CircuitoDeVivencias
     config.load_defaults 5.2
     config.i18n.default_locale = :'pt-BR'
     config.autoload_paths += %W[#{config.root}/lib]
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
