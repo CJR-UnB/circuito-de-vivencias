4a3a2cbbc7baaeb94bc477654b60c824d9435523
Thiago <thiagobarros@cjr.org.br>
2018-11-16 18:08:04 -0200

    arrumando o seed da terceira role, trocando de create para new

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Role.delete_all
+User.delete_all
 adminRole = Role.new(name: 'Admin')
 adminRole.id = 1
 adminRole.save!
@@ -16,16 +17,14 @@ supervisorRole = Role.new(name: 'Supervisor')
 supervisorRole.id = 2
 supervisorRole.save!
 
-userRole = Role.create(name: 'User')
+userRole = Role.new(name: 'User')
 userRole.id = 3
 userRole.save!
 
-User.where(name: 'Admin').destroy_all
 admin = User.new(name: 'Admin', surname: 'Admin', cpf: '123.456.789-09', email: 'admin@email.com', password: '12345678')
 admin.turnIntoAdmin
 admin.save
 
-User.where(name: 'Supervisor').destroy_all
 supervisor = User.new(name: 'Supervisor', surname: 'Supervisor', cpf: '11111111111', email: 'supervisor@email.com', password: '12345678')
 supervisor.turnIntoSupervisor
 supervisor.save
