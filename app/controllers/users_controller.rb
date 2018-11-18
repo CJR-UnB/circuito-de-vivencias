fb9119407031ecb6b21c1adf5e6e4bb0362919e7
Vinícius Gomes <vgdsouza@live.com>
2018-11-18 18:28:28 -0200

    template implementado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:profile, :rejected_workshops]
 
-  def show
-  end
-  def profile
-  end
+  def show; end
+
+  def profile; end
+
   def rejected_workshops
     @workshops = Workshop.where({ author_id: current_user.id, status: "rejected" } )
   end
