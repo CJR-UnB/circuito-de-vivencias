3f789b377e190c6ce34b6b3516914f2f7ea804e4
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2018-12-05 10:40:57 -0200

    feita a pagina de oficinas e mudancas no model de oficinas

--- app/models/category.rb
+++
@@ -1,4 +0,0 @@
-class Category < ApplicationRecord
-  has_many :workshop_categories
-  has_many :workshops, through: :workshop_categories
-end
--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -8,10 +8,7 @@ class Workshop < ApplicationRecord
   has_one :feedback
   has_many :users, through: :evaluations
   has_many :comments
-
-  has_many :workshop_categories
-  has_many :categories, through: :workshop_categories, source: :category
-
+  
   validates :title, presence: true
   validates :document, presence: true, blob: {content_type: ['application/vnd.oasis.opendocument.text', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']}
 
