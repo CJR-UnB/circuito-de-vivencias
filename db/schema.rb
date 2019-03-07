dd707945d8dc294ffc563f7ed6916f4e35d8f31d
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2019-03-07 00:17:51 -0300

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_100430) do
+ActiveRecord::Schema.define(version: 2019_02_20_191538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,8 @@ ActiveRecord::Schema.define(version: 2018_12_14_100430) do
     t.string "surname"
     t.string "cpf"
     t.integer "role_id", default: 3
+    t.string "profession"
+    t.string "institution"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
