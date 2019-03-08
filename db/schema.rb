856c079b2ac4bac32625df9b2fe1147b07cea7b0
Thiago <thiagobarros@cjr.org.br>
2019-03-08 15:01:53 -0300

    Rodando migration e testando alteraçoes de nome

--- db/schema.rb
+++ db/schema.rb
@@ -151,6 +151,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_223649) do
     t.integer "role_id", default: 3
     t.string "profession"
     t.string "institution"
+    t.bigint "state_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
