4ad68d1bcc403f6f07233204882d66305452b5bf
JoaoGabriel0511 <joaogabrie0511@gmail.com>
2019-03-31 09:20:55 -0300

    migrate ajustes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_201900) do
+ActiveRecord::Schema.define(version: 2019_03_30_214729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,10 +149,10 @@ ActiveRecord::Schema.define(version: 2019_03_30_201900) do
     t.string "surname"
     t.string "cpf"
     t.integer "role_id", default: 3
-    t.string "profession"
     t.string "institution"
     t.integer "state"
     t.integer "institution_type"
+    t.string "profession"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
